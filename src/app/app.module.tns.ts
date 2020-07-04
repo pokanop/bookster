8b1a5c193892be8c499560696fab62713feac504
Sahel Jalal <sahel.jalal@icloud.com>
2020-07-03 21:32:32 -0700

    Add empty components and link routes to other pages

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -4,6 +4,10 @@ import { NativeScriptModule } from '@nativescript/angular';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { LibraryComponent } from '@src/app/library/library.component';
+import { BooksComponent } from '@src/app/books/books.component';
+import { FriendsComponent } from '@src/app/friends/friends.component';
+import { ProfileComponent } from '@src/app/profile/profile.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding and/or HTTP wrapper
@@ -13,6 +17,10 @@ import { HomeComponent } from '@src/app/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    LibraryComponent,
+    BooksComponent,
+    FriendsComponent,
+    ProfileComponent,
   ],
   imports: [
     NativeScriptModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,31 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from '@src/app/home/home.component';
+import { LibraryComponent } from './library/library.component';
+import { BooksComponent } from './books/books.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendsComponent } from './friends/friends.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'library',
+    component: LibraryComponent,
+  },
+  {
+    path: 'books',
+    component: BooksComponent,
+  },
+  {
+    path: 'friends',
+    component: FriendsComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
   },
 ];
