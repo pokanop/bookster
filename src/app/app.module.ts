d763319a7a1452182a07ce6861bf77534c0e9344
Sahel Jalal <sahel.jalal@icloud.com>
2020-07-04 00:28:54 -0700

    Change to FlexLayout for Angular instead for book grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 
@@ -34,12 +34,12 @@ import { BookCardComponent } from '@src/app/components/book-card/book-card.compo
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatGridListModule,
     MatMenuModule,
     MatCardModule,
     AppRoutingModule,
