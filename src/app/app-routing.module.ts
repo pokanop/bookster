e73abee43492631eb70210470a9013f32bcf0b0e
Sahel Jalal <sahel.jalal@icloud.com>
2020-07-07 19:47:20 -0700

    Fix scrolling to top on route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,47 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {
+  RouterModule,
+  Router,
+  RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+} from '@angular/router';
 import { routes } from '@src/app/app.routes';
+import { filter } from 'rxjs/operators';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  //
+  // Sadly Angular's built-in scrolling doesn't work properly so this is a workaround:
+  // See these for issue and workarounds:
+  // - https://github.com/angular/angular/issues/24547
+  // - https://github.com/angular/components/issues/11552
+  //
+  scrollTopPositions: { [url: string]: number } = {};
+
+  constructor(router: Router) {
+    router.events
+      .pipe(
+        filter(
+          (event: RouterEvent) =>
+            event instanceof NavigationStart || event instanceof NavigationEnd
+        )
+      )
+      .subscribe({
+        next: (event: RouterEvent) => {
+          const scrollContainer = document.querySelector('mat-sidenav-content');
+          if (event instanceof NavigationStart) {
+            this.scrollTopPositions[router.url] = scrollContainer.scrollTop;
+          } else if (event instanceof NavigationEnd) {
+            const newUrl = router.url;
+            setTimeout(() => {
+              scrollContainer.scrollTop = this.scrollTopPositions[newUrl];
+            }, 0);
+          }
+        },
+      });
+  }
+}
