0ede00b9a00fa34b1072c36a68503775ba1f79a5
Sahel Jalal <sahel.jalal@icloud.com>
2020-07-08 01:11:05 -0700

    Add common security middleware to nest server

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(helmet());
+  app.enableCors();
   await app.listen(process.env.PORT || 4200);
 }
 
